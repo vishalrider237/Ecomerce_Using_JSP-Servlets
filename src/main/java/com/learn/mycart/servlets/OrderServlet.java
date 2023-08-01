b5c74ab51e7b04e38917a5f638136e96084107dc
Vishal Pandey <vishalpandey10022000@gmail.com>
2023-08-01 19:51:29 +0530

    first commit

---
+++ src/main/java/com/learn/mycart/servlets/OrderServlet.java
@@ -0,0 +1,37 @@
+package com.learn.mycart.servlets;
+
+import com.learn.mycart.helper.FactoryProvider;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "OrderServlet",value = "/OrderServlet")
+public class OrderServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession httpSession= request.getSession();
+            httpSession.setAttribute("message","Order Successful!!");
+            response.sendRedirect("checkout.jsp");
+
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req,resp);
+    }
+}
